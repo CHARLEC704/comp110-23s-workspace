3bc11b42a6a941643ee1bf48e09e8b3e39e0fec0
Charles Harris <charris1@unc.edu>
2023-04-04 13:15:08 -0400

    .

---
+++ exercises/Lessons/conditionals.py
@@ -0,0 +1,8 @@
+"""Checks light, if green, says go."""
+
+light: str = input("What color is the light?")
+
+if (light == "green"):
+    print("Go!")
+else:
+    print("Stop!")
--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,8 +1,8 @@
 """My first program for COMP110."""
-_author_ = "740461000"
+
+__author__ = "740461000"
+
 print("Hello world, I'm Charles")
 
-
-
-
-
+print(3+1)
+print("hello")
